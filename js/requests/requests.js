0c7255abcc3d2c215f4f8d9e483eda32f8e66d44
ericluciano <ericluciano.rj@gmail.com>
2020-01-02 18:58:49 -0300

    add api url

--- js/requests/requests.js
+++ js/requests/requests.js
@@ -1,6 +1,6 @@
 import { render } from '../render.js';
 
-const API = 'http://localhost:9000/api/';
+const API = `${window.location.href}api/`;
 
 const fetchVerifySequence = ({ id, action, numbers }) => {
     
